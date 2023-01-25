0dc966329a4fbac4734ae28da0743fefbfc43fa4
Yosef Teshome <menelik08@gmail.com>
2023-01-25 16:20:52 -0500

    file structure

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home'
 import About from './pages/About/About';
 import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
-import Footer from '../Footer/Footer'
+import Footer from './Footer/Footer'
 
  function Dashboard() {
     const [currentPage, setCurrentPage] = useState('Home');
